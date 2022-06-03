5f8130744e01aa1ede7b670a57ccfc21c0354436
Thalmenoch <thalmenoch@gmail.com>
2022-06-02 23:05:06 -0300

    pegar_comparador(incompleto)
    embrionário da obtenção de m

--- src/entrada.py
+++ src/entrada.py
@@ -3,6 +3,22 @@ import pandas as pd
 import ferramentas
 
   #altere o endereço de acordo com localização do arquivo
-df = pd.read_csv(r'C:\\Users\Luciano\Desktop\Workspace\Python\Projeto I\\Arquivos - Pacientes\column_bin_3a_3p.csv')  
+df = pd.read_csv(r'C:\\Users\Luciano\Desktop\Workspace\Python\Projeto I\\Arquivos - Pacientes\column_bin_3a_2p.csv')  
 
-print(df)
+#print(df)
+"""
+def pegar_comparador(arquivo):
+    tabela = []
+    for linha in range(len(arquivo)):
+        for coluna in range(len(arquivo)):
+            if coluna != 'P':
+                tabela = arquivo[0][coluna]
+    return tabela
+"""
+
+
+               
+
+
+
+
