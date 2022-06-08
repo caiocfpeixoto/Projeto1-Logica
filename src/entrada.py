e482f4ec93638fc37371710b1662c37b2f0ba9a8
Thalmenoch <thalmenoch@gmail.com>
2022-06-07 23:05:46 -0300

    up

--- src/entrada.py
+++ src/entrada.py
@@ -5,7 +5,9 @@ import ferramentas
   #altere o endereço de acordo com localização do arquivo
 df = pd.read_csv(r'C:\\Users\Luciano\Desktop\Workspace\Python\Projeto I\\Arquivos - Pacientes\column_bin_3a_2p.csv')  
 
-#print(df)
+
+
+print(df)
 """
 def pegar_comparador(arquivo):
     tabela = []
@@ -15,6 +17,8 @@ def pegar_comparador(arquivo):
                 tabela = arquivo[0][coluna]
     return tabela
 """
+#def ret1(arquivo):
+    
 
 
                
