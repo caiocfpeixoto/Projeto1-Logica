0375e33b86bf5fff2fc559c28fc9507ebed30dd9
Caio Cesar Ferreira Peixoto Machado <63310044+caiocfpeixoto@users.noreply.github.com>
2022-06-10 22:49:34 -0300

    final boss

--- src/test.py
+++ src/test.py
@@ -252,32 +252,36 @@ def unionDic(dic1, dic2):
 ##############################################################################
 
 #Checagem na situação em que a não está em i
-def att_check(arquivo):
-  if 'PI' in arquivo:
-    #print(f'PI está presente -> {arquivo[n]}')
-    check = 'PI'
-   
-  if 'GS' in arquivo:
-    #print(f'GS está presente -> {arquivo[n]}') 
-    check = 'GS'
+def contagem(arquivo):
+  list = [0, 0, 0, 0, 0, 0]
+  m = 0
+  for a in range(len(arquivo)):
+    if 'PI' in arquivo[a]:
+      if list[0] == 0:
+        m += 1
+        list[0] += 1
+    if 'GS' in arquivo[a]:
+      if list[1] == 0:
+        m += 1
+        list[1] += 1
+    if 'PT' in arquivo[a]:
+      if list[2] == 0:
+        m += 1 
+        list[2] += 1  
+    if 'LA' in arquivo[a]:
+      if list[3] == 0:
+        m += 1
+        list[3] += 1
+    if 'SS' in arquivo[a]:
+      if list[4] == 0:
+        m += 1  
+        list[4] += 1
+    if 'RP' in arquivo[a]:
+      if list[5] == 0:
+        m += 1 
+        list[5] += 1 
 
-  if 'PT' in arquivo:
-     #print(f'PT está presente -> {arquivo[n]}')  
-    check = 'PT'
-
-  if 'LA' in arquivo:
-    #print(f'LA está presente -> {arquivo[n]}')
-    check = 'LA'
-
-  if 'SS' in arquivo:
-    #print(f'SS está presente -> {arquivo[n]}')
-    check = 'SS'
-
-  if 'RP' in arquivo:
-    #print(f'RP está presente -> {arquivo[n]}')  
-    check = 'RP'
-  
-  return check   
+  return m   
 
 #(X_a_i_p) V (X_a_i_n) V (X_a_i_s)
 #df.columns é o se utiliza pra contar as chaves(ex=PI > x) das colunas
@@ -287,29 +291,51 @@ def att_check(arquivo):
 #restricão 1
 def ret1(arquivo, regra):
   list_row = []
-  for atributo in (range(len(arquivo)-1)):
-    for i in range(len(regra)):
-      if att_check(arquivo[atributo]) in regra[i] :
-        if regra[i] == arquivo[atributo]:
-          list_row.append(f'X_{arquivo[atributo]}_{str(i+1)}_p')
-        elif (regra[i] != arquivo[atributo]):
-          list_row.append(f'X_{arquivo[atributo]}_{str(i+1)}_n')
-      else:
-        list_row.append(f'X_{arquivo[atributo]}_{str(i+1)}_s')  
-      
-  return list_row 
+  for i in range(len(regra)):  
+    list_atoms = []
+    for a in (range(len(arquivo)-1)):
+      list_atom=[]
+      for aux in range(3):
+        list_aux = []
+        if aux == 0:
+          list_aux.append(Atom('X_'+arquivo[a]+'_'+str(i+1)+'_p'))
+          list_aux.append(Not('X_'+arquivo[a]+'_'+str(i+1)+'_n'))
+          list_aux.append(Not('X_'+arquivo[a]+'_'+str(i+1)+'_s'))
+          
+          
+        if aux == 1:
+          list_aux.append(Not('X_'+arquivo[a]+'_'+str(i+1)+'_p'))
+          list_aux.append(Atom('X_'+arquivo[a]+'_'+str(i+1)+'_n'))
+          list_aux.append(Not('X_'+arquivo[a]+'_'+str(i+1)+'_s'))
+            
+        if aux == 2:
+          list_aux.append(Not('X_'+arquivo[a]+'_'+str(i+1)+'_p'))
+          list_aux.append(Not('X_'+arquivo[a]+'_'+str(i+1)+'_n'))
+          list_aux.append(Atom('X_'+arquivo[a]+'_'+str(i+1)+'_s'))
+        
+        list = and_all(list_aux) 
+        list_atom.append(list)
+        
+      list = or_all(list_atom) 
+      list_atoms.append(list)
+
+    list = and_all(list_atoms)
+    list_row.append(list)  
+
+  return and_all(list_row) 
 ##############################################################################
 
 #restricão 2
 def ret2(arquivo, regra):
   list_row=[]
   for i in (range(len(regra))):
+    list_atom = []
     for a in (range(len(arquivo)-1)):
-      if att_check(regra[i]) in arquivo[a]:
-        list_row.append(Not(f'X_{arquivo[a]}_{str(i+1)}_s'))
-      else:
-        list_row.append((f'X_{arquivo[a]}_{str(i+1)}_s'))
-  return or_all(list_row)
+      list_atom.append(Not(f'X_{arquivo[a]}_{str(i+1)}_s'))
+    list = or_all(list_atom)
+    list_row.append(list)
+  
+  return and_all(list_row) 
 ##############################################################################
 #(xPI≤42.09,1,p ∨ xPI≤70.62,1,p ∨ xPI≤80.61,1,p ∨ xGS≤37.89,1,n ∨ xGS≤57.55,1,n)
 #(xPI≤42.09,2,p ∨ xPI≤70.62,2,p ∨ xPI≤80.61,2,p ∨ xGS≤37.89,2,n ∨ xGS≤57.55,2,n)
@@ -322,12 +348,10 @@ def ret3(arquivo,regra):
     for i in range(len(regra)):
             list_atom=[]
             for a in range(len(arquivo)-1):
-                    if regra[i] == arquivo[a]:
+                    if regra[i] == arquivo[a][a]:
                         list_atom.append('X_'+str(arquivo[a])+'_'+ str(i+1)+'_'+'p')
-                    elif (regra[i] != arquivo[a]):
+                    elif (regra[i] != arquivo[a][a]):
                         list_atom.append('X_'+str(arquivo[a])+'_'+ str(i+1)+'_'+'n')
-                    else:
-                        list_atom.append('X_'+str(arquivo[a])+'_'+ str(i+1)+'_'+'s')
             list=or_all(list_atom)
             list_rows.append(list)
     return and_all(list_rows)        
@@ -369,8 +393,7 @@ def ret5(arquivo,regra):
         list=or_all(list_atom)
         list_rows.append(list)  
     return and_all(list_rows)
-m = [0,1 ]
-print(ret3(df.columns,m))
+
 ##############################################################################
 
 #Solução
@@ -401,7 +424,8 @@ def patologia_solucao(arquivo,regra):
     return print(list_atom+'P')
   else:
       print('Paciente'+str(j+1)+'não tem patalogia')
-
+regra3 = ['PI > 42.09', 'LA > 39.63', 'GS > 37.89']
+print(patologia_solucao(df.columns,regra3))
 #############################################################################
 
 
